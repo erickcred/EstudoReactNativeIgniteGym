48e458ec5672aaf8d0056b402f58b18718db35ea
Erick Rick <erickcredjes@gmail.com>
2025-01-13 21:14:01 -0300

    Finalizando inputs

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,23 +1,30 @@
-import { Input as InputC, InputField } from '@gluestack-ui/themed';
+import { Input as GluestackInput, InputField } from '@gluestack-ui/themed';
 import { ComponentProps } from 'react';
 
 type Props = ComponentProps<typeof InputField>
 
 export function Input({ ...rest }: Props) {
   return (
-    <InputC
+    <GluestackInput
       bg="$gray700"
       h="$14"
       px="$4"
-      borderWidth="$0"
+      borderWidth="$1"
+      borderColor="transparent"
       borderRadius="$md"
+      $focus={{
+        bgColor: '$gray600',
+        borderColor: '$green500',
+        borderWidth: '$1',
+        borderRadius: '$md',
+      }}
     >
       <InputField
+        { ...rest }
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"
-        { ...rest }
       />
-    </InputC>
-  );
+    </GluestackInput>
+  ); 
 }
--- src/screens/SignIn/signIn.tsx
+++ src/screens/SignIn/signIn.tsx
@@ -2,8 +2,12 @@ import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed";
 import BackgroundImg from '@assets/background.png';
 import Logo from '@assets/logo.svg';
 import { Input } from "@components/Input/input";
+import { useState } from "react";
 
 export function SignIn() {
+  const [ email, setEmail ] = useState('');
+  const [ senha, setSenha ] = useState('');
+
   return (
     <VStack flex={ 1 } bg="$gray700">
       <Image
@@ -24,8 +28,23 @@ export function SignIn() {
         <Center gap="$2">
           <Heading color="$gray100">Acesse a conota</Heading>
 
-          <Input placeholder="E-mail"></Input>
-          <Input placeholder="Senha"></Input>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            onChangeText={ setEmail }
+            onSubmitEditing={ () => console.log('submit', email) }
+            onKeyPress={ () => console.log('keypress', email) }
+            value={ email }
+          />
+          <Input
+            placeholder="Senha"
+            secureTextEntry={ true }
+            onChangeText={ setSenha }
+            onSubmitEditing={ () => console.log('submit', senha) }
+            onKeyPress={ () => console.log('keypress', senha) }
+            value={ senha }
+          />
         </Center>
       </VStack>
     </VStack>
