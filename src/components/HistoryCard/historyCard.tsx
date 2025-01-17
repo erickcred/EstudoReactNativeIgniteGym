d997e4feec5f53ade66fc0cec5960b21d4085df4
Erick Rick <erickcredjes@gmail.com>
2025-01-16 21:09:42 -0300

    Criando component HistoryCard

---
+++ src/components/HistoryCard/historyCard.tsx
@@ -0,0 +1,40 @@
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+
+export function HistoryCard() {
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      mb="$3"
+      bg="$gray600"
+      rounded="$md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack
+        mr="$5"
+      >
+        <Heading
+          color="$white"
+          fontFamily="$heading"
+          fontSize="$md"
+          textTransform="capitalize"
+        >
+          Costas
+        </Heading>
+
+        <Text
+          color="$gray100"
+          fontFamily="$body"
+          fontSize="$lg"
+          numberOfLines={ 2 }
+        >
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="$gray300" fontSize="$md">08:00</Text>
+    </HStack>
+  )
+}
