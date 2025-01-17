a05a48e7f712e204f2e04214dcb247d8d1a0bd39
Erick Rick <erickcredjes@gmail.com>
2025-01-16 21:09:07 -0300

    Criando component screenHeader

---
+++ src/components/ScreenHeader/screenHeader.tsx
@@ -0,0 +1,23 @@
+import { Center, Heading } from "@gluestack-ui/themed";
+
+type Props = {
+  title: string;
+}
+
+export function ScreenHeader({ title }: Props) {
+  return (
+    <Center
+      bg="$gray600"
+      pt="$16"
+      pb="$6"
+    >
+      <Heading
+        color="$gray100"
+        fontSize="$xl"
+        fontFamily="$heading"
+      >
+        { title }
+      </Heading>
+    </Center>
+  )
+}
