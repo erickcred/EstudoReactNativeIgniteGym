ae5e3307a319f90260873b61c095077a0dbe2e04
Erick Rick <erickcredjes@gmail.com>
2025-01-25 09:03:08 -0300

    Finalizando layout Profiles

--- src/screens/Internal/Profile/profile.tsx
+++ src/screens/Internal/Profile/profile.tsx
@@ -1,9 +1,10 @@
 import { ScrollView, TouchableOpacity } from 'react-native';
-import { Center, VStack, Text } from '@gluestack-ui/themed';
+import { Center, VStack, Text, Heading } from '@gluestack-ui/themed';
 
 import { ScreenHeader } from '@components/internal/ScreenHeader/screenHeader';
 import { UserPhoto } from '@components/internal/UserPhoto/userPhoto';
 import { Input } from '@components/Input/input';
+import { Button } from '@components/Button/button';
 
 export function Profile() {
   return (
@@ -41,6 +42,41 @@ export function Profile() {
               value="erick@email.com"
               bg="$gray600"
               isReadOnly
+            />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$16"
+            mb="$8"
+          >Alterar senha</Heading>
+
+          <Center
+            w="$full"
+            gap="$4"
+          >
+            <Input
+              placeholder="Senha antiga"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Input
+              placeholder="Nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+            
+            <Button
+              title="Atualizar"
             />
           </Center>
         </Center>
