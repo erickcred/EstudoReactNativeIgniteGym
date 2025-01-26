8f4538d9f6adbd1d22ae7a524e04956f8f5e8d6d
Erick Rick <erickcredjes@gmail.com>
2025-01-26 14:25:27 -0300

    Aplicando interação com o dispositivo para selecionar imagem da galeria utilizando (expo-image-picker e expo-file-system

--- src/screens/Internal/Profile/profile.tsx
+++ src/screens/Internal/Profile/profile.tsx
@@ -1,12 +1,71 @@
+import { useState } from 'react';
 import { ScrollView, TouchableOpacity } from 'react-native';
-import { Center, VStack, Text, Heading } from '@gluestack-ui/themed';
+
+import { Center, VStack, Text, Heading, useToast } from '@gluestack-ui/themed';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
 
 import { ScreenHeader } from '@components/internal/ScreenHeader/screenHeader';
 import { UserPhoto } from '@components/internal/UserPhoto/userPhoto';
 import { Input } from '@components/Input/input';
 import { Button } from '@components/Button/button';
+import { ToastMessage } from '@components/ToastMessage/toastMessage';
 
 export function Profile() {
+  const [ userPhoto, setUserPhoto ] = useState<string>('https://github.com/erickcred.png');
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [ 4, 4 ],
+        allowsEditing: true,
+      });
+  
+      if (photoSelected.canceled) return;
+  
+      const photoUri = photoSelected.assets[0].uri
+      if (photoUri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoUri) as { size: number };
+        
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 0.05) { // Validação para ver se a foto é maior que 5mb
+          toast.show({
+            placement: 'top',
+            render: ({ id }) => (
+              <ToastMessage
+                id={ id }
+                title="Erro!"
+                description="Essa imagem é muito grando. Escolha uma de até 5MB."
+                action="error"
+                onClose={ () => toast.close(id) }
+              />
+            ),
+            duration: 10000,
+          });
+          return;
+        }
+  
+        setUserPhoto(photoUri);
+      }
+    } catch (error) {
+      toast.show({
+        render: ({ id }) => (
+          <ToastMessage
+            id={ id }
+            title="Não foi possível carregar a imagem."
+            action="error"
+            onClose={ () => toast.close(id) }
+          />
+        )
+      });
+      console.log(error);
+    }
+
+  }
+
   return (
     <VStack flex={ 1 }>
       <ScreenHeader title="Perfil" />
@@ -19,11 +78,11 @@ export function Profile() {
           px="$10"
         >
           <UserPhoto
-            source={{ uri: 'https://github.com/erickcred.png' }}
+            source={{ uri: userPhoto }}
             size="xl"
             alt="Erick Rick"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={ handleUserPhotoSelect }>
             <Text
               color="$green500"
               fontFamily="$heading"
